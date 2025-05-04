960a3b343e00393abd8f5c4f6b400c04858bfc7a
isravarg <israel.vargas@magna.com>
2025-05-04 13:31:01 -0600

    dev

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,15 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AdministratorComponent } from './components/administrator/administrator.component';
 import { CreateStockComponent } from './components/create-stock/create-stock.component';
 import { EditStockComponent } from './components/edit-stock/edit-stock.component';
+import { authenticationGuard } from './core/authentication/authentication.guard';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    { path: "", redirectTo: "home", pathMatch: "full" },
-    { path: "home", component: HomeComponent },
-    { path: "dashboard", component: DashboardComponent },
-    { path: "administrator", component: AdministratorComponent },
-    { path: "administrator/stocks/create", component: CreateStockComponent }, 
-    { path: "administrator/stocks/edit/:id", component: EditStockComponent }, 
+    { path: "", redirectTo: "login", pathMatch: "full" },
+    { path: "home", component: HomeComponent, canActivate: [authenticationGuard] },
+    { path: "dashboard", component: DashboardComponent, canActivate: [authenticationGuard] },
+    { path: "administrator", component: AdministratorComponent, canActivate: [authenticationGuard] },
+    { path: "administrator/stocks/create", component: CreateStockComponent, canActivate: [authenticationGuard] },
+    { path: "administrator/stocks/edit/:id", component: EditStockComponent, canActivate: [authenticationGuard] },
+    { path: "login", component: LoginComponent },
 ];
