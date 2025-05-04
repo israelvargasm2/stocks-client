e5e279656c0098ce14d32ed59fa971af08b5a8c7
Vargas <Israel.Vargas@magna.com>
2025-05-04 13:09:39 -0600

    dev

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authenticationInterceptor } from './core/authentication/authentication.interceptor';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([authenticationInterceptor])
-    ),
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withFetch())]
 };
